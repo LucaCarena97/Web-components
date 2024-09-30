0272d21dcf30d874ba9e64f4f34679a92af05c88
lucacarena97 <lucacarena98@gmail.com>
2024-09-30 20:19:02 -0300

    img

--- components/header.js
+++ components/header.js
@@ -1,24 +1,25 @@
 function header() {
   const header = document.querySelector(".nav");
   header.innerHTML = `
-    <a href="../index.html">
-    <img  class="logoHeader" src="../images/Logo.jpeg" alt="Logo"/></a>
+    <a href="/Web-components/index.html">
+      <img class="logoHeader" src="/Web-components/images/Logo.jpeg" alt="Logo"/>
+    </a>
 
-     <div class="menu_a_desk">
-      <a href="../portfolio.html">Portfolio</a>
-      <a href="../servicios.html">Servicios</a>
-      <a href="../contacto.html">Contacto</a>
+    <div class="menu_a_desk">
+      <a href="/Web-components/portfolio.html">Portfolio</a>
+      <a href="/Web-components/servicios.html">Servicios</a>
+      <a href="/Web-components/contacto.html">Contacto</a>
     </div>
     <button class="menu-button">
-      <img class="menu-icon" src="../images/menu.png" alt="Menu"/>
+      <img class="menu-icon" src="/Web-components/images/menu.png" alt="Menu"/>
     </button>
     <div class="menu-content hidden">
-    <button class="close-button">X</button>
-    <div class="menu_a">
-      <a href="../portfolio.html">Portfolio</a>
-      <a href="../servicios.html">Servicios</a>
-      <a href="../contacto.html">Contacto</a>
-    </div>
+      <button class="close-button">X</button>
+      <div class="menu_a">
+        <a href="/Web-components/portfolio.html">Portfolio</a>
+        <a href="/Web-components/servicios.html">Servicios</a>
+        <a href="/Web-components/contacto.html">Contacto</a>
+      </div>
     </div>
   `;
 
