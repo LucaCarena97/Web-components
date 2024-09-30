20619bd74d9a98acdb9bd5cb4408c71fa8113b65
lucacarena97 <lucacarena98@gmail.com>
2024-09-30 20:36:44 -0300

    img footer

--- components/footer.js
+++ components/footer.js
@@ -12,17 +12,17 @@ function footer() {
     <a href="./servicios.html">Servicios</a>
   </div>
   <div>
-    <img class="iconos" src="../images/phone.png" alt="contacto"/>
+    <img class="iconos" src="./images/phone.png" alt="contacto"/>
     <a href="./contacto.html">Contacto</a>
   </div>
 </section>
 
 <section class="redes">
   <a href="https://www.linkedin.com/in/luca-carena-463855127/">
-  <img class="redes" src="../images/link.png" alt="link"/></a>
+  <img class="redes" src="./images/link.png" alt="link"/></a>
 
   <a href="https://github.com/LucaCarena97">
-  <img class="redes" src="../images/git.png" alt="git"/></a>
+  <img class="redes" src="./images/git.png" alt="git"/></a>
 </section>
 
 <p>©2022 - https://apx.school</p>
